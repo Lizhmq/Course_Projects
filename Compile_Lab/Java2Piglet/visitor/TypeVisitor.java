ac0cd60f5546ec1b4a2555121bdd18b2108c359b
Joe Lee <lizhmq@pku.edu.cn>
2019-04-22 17:12:37 +0800

    add TypeVisitor

---
+++ Compile_Lab/Java2Piglet/visitor/TypeVisitor.java
@@ -0,0 +1,76 @@
+package visitor;
+import syntaxtree.*;
+import java.util.*;
+import symbol.*;
+
+
+// return value: String is the type of TreeNode
+public class TypeVisitor extends GJDepthFirst<String, MType> {
+	
+	/* 
+	 * 
+	 */
+        MClasses global;
+        TypeVisitor(MClasses g) { global = g; }
+        public String visit(PrimaryExpression n, MType env) {
+                return n.f0.accept(this, env);
+        }
+        public String visit(IntegerLiteral n, MType env) {
+                return "int";
+        }
+        public String visit(TrueLiteral n, MType env) {
+                return "boolean";
+        }
+        public String visit(FalseLiteral n, MType env) {
+                return "boolean";
+        }
+        public String visit(NotExpression n, MType env) {
+                return "boolean";
+        }
+        public String visit(Identifier n, MType env) {
+                return n.f0.toString();
+        }
+        public String visit(ThisExpression n, MType env) {
+                MMethod method = (MMethod)env;
+                MClass c = method.owner;
+                return c.name;
+        }
+        public String visit(BracketExpression n, MType env) {
+                return n.f1.accept(this, env);
+        }
+        public String visit(ArrayAllocationExpression n, MType env) {
+                return "int[]";
+        }
+        public String visit(AllocationExpression n, MType env) {
+                return n.f1.f0.toString();
+        }
+        public String visit(Expression n, MType env) {
+                return n.f0.accept(this, env);
+        }
+        public String visit(AndExpression n, MType env) {
+                return "boolean";
+        }
+        public String visit(CompareExpression n, MType env) {
+                return "boolean";
+        }
+        public String visit(PlusExpression n, MType env) {
+                return "int";
+        }
+        public String visit(MinusExpression n, MType env) {
+                return "int";
+        }
+        public String visit(TimesExpression n, MType env) {
+                return "int";
+        }
+        public String visit(ArrayLookup n, MType env) {
+                return "int";
+        }
+        public String visit(ArrayLength n, MType env) {
+                return "int";
+        }
+        public String visit(MessageSend n, MType env) {
+                MClass c = global.queryClass(n.f0.accept(this, env));
+                MMethod method = c.queryMethod(n.f2.toString());
+                return method.returnType;
+        }
+}
