21928b6750a9af9040b3f87358cdaa474bdd0196
Joe Lee <lizhmq@pku.edu.cn>
2019-05-06 22:43:55 +0800

    add clean

--- Compile_Lab/Piglet2SPiglet/Main.java
+++ Compile_Lab/Piglet2SPiglet/Main.java
@@ -1,16 +1,19 @@
 import java.io.*;
-import symbol.*;
+
+import visitor.SpigletVisitor;
 import visitor.*;
 import syntaxtree.*;
 import java.util.*;
-import utils.*
+import utils.*;
 
 public class Main {
 	public static void main(String args[]) {
 		try {
 			InputStream in = new FileInputStream(args[0]);
-			Node root = new SpigletParser(in).Goal();
-			SpigletResult res = root.accept(new SpigletVisitor(), null);
+			Node root = new PigletParser(in).Goal();
+			SpigletVisitor vis = new SpigletVisitor();
+			SpigletResult res = root.accept(vis);
+			String spig = vis.result.toString();
 			String outputfile = null;
 			if (args.length > 1) {
 				outputfile = args[1];
@@ -18,7 +21,7 @@ public class Main {
 				outputfile = "Spiglet.txt";
 			}
 			OutputStream out = new FileOutputStream(outputfile);
-			out.write(res.toString().getBytes());
+			out.write(spig.getBytes());
 			System.out.println(String.format("Spiglet code generated -- \"%s\".", outputfile));
 		} catch (Exception e) {
 			e.printStackTrace();
