4c8bc824578ff478258beb4f4b0b4d14f7344777
Joe Lee <lizhmq@pku.edu.cn>
2019-05-07 23:38:09 +0800

    fix bug

--- Compile_Lab/Piglet2SPiglet/visitor/MaxTempVisitor.java
+++ Compile_Lab/Piglet2SPiglet/visitor/MaxTempVisitor.java
@@ -7,7 +7,7 @@ import utils.*;
 public class MaxTempVisitor extends GJVoidDepthFirst<Integer> {
 
         public int MaxTemp = 0;
-        public void visit(Temp n, int p) {
+        public void visit(Temp n, Integer p) {
                 MaxTemp = Math.max(Integer.parseInt(n.f1.f0.tokenImage), MaxTemp);
         }
 }
