42fffe0b229fd06fc0ef00a5eb8a78ab2072e3c9
Joe Lee <lizhmq@pku.edu.cn>
2019-05-21 19:39:32 +0800

    init Spiglet2Kanga

---
+++ Compile_Lab/Spiglet2Kanga/Main.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+import visitor.*;
+import syntaxtree.*;
+import java.util.*;
+import utils.*;
+
+// Usage:	java Main sample.pg out.spg
+
+public class Main {
+	public static void main(String args[]) {
+		try {
+			InputStream in = new FileInputStream(args[0]);
+			Node root = new PigletParser(in).Goal();
+			MaxTempVisitor tmpVis = new MaxTempVisitor();
+			root.accept(tmpVis, 0);
+			SpigletVisitor vis = new SpigletVisitor(tmpVis.MaxTemp + 1);
+			SpigletResult res = root.accept(vis, false);
+			String spig = vis.result.toString();
+			String outputfile = null;
+			if (args.length > 1) {
+				outputfile = args[1];
+			} else {
+				outputfile = "Spiglet.txt";
+			}
+			OutputStream out = new FileOutputStream(outputfile);
+			out.write(spig.getBytes());
+			System.out.println(String.format("Spiglet code generated -- \"%s\".", outputfile));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
